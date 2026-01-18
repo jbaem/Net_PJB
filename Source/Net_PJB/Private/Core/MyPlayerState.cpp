538ca8229cf9161d7d61bc375f4cd1cc2616fb69
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2026-01-18 20:28:21 +0900

    [feat] 플레이어 스테이트 연결

--- Source/Net_PJB/Private/Core/MyPlayerState.cpp
+++ Source/Net_PJB/Private/Core/MyPlayerState.cpp
@@ -1,2 +1,118 @@
 #include "Core/MyPlayerState.h"
 
+#include "Net/UnrealNetwork.h"
+
+#include "Player/MyCharacter.h"
+#include "Blueprint/UserWidget.h"
+#include "UI/MyHUD.h"
+#include "UI/MyMainWidget.h"
+
+AMyPlayerState::AMyPlayerState()
+{
+	bReplicates = true;
+}
+
+void AMyPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ResetMyScore();
+}
+
+void AMyPlayerState::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (OnScoreChangedDel.IsBound())
+	{
+		OnScoreChangedDel.Clear();
+	}
+
+	if (OnNameChangedDel.IsBound())
+	{
+		OnNameChangedDel.Clear();
+	}
+
+	if (OnStateChangedDel.IsBound())
+	{
+		OnStateChangedDel.Clear();
+	}
+
+	Super::EndPlay(EndPlayReason);
+}
+
+void AMyPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AMyPlayerState, ReadyType);
+	DOREPLIFETIME(AMyPlayerState, MyScore);
+	DOREPLIFETIME(AMyPlayerState, MyName);
+}
+
+void AMyPlayerState::SetReadyType(EReadyType NewReadyType)
+{
+	if(HasAuthority())
+	{
+		ReadyType = NewReadyType;
+		OnStateChangedDel.Broadcast(ReadyType);
+		OnRep_ReadyType();
+	}
+}
+
+void AMyPlayerState::AddMyScore(int32 ScoreToAdd)
+{
+	if (HasAuthority())
+	{
+		MyScore += ScoreToAdd;
+		OnScoreChangedDel.Broadcast(MyScore);
+		OnRep_MyScore();
+	}
+
+}
+
+void AMyPlayerState::ResetMyScore()
+{
+	if(HasAuthority())
+	{
+		MyScore = 0;
+		OnScoreChangedDel.Broadcast(MyScore);
+		OnRep_MyScore();
+	}
+}
+
+void AMyPlayerState::SetMyName(const FString& NewName)
+{
+	if(HasAuthority())
+	{
+		MyName = NewName;
+		OnNameChangedDel.Broadcast(MyName);
+		OnRep_MyName();
+	}
+}
+
+void AMyPlayerState::OnRep_ReadyType()
+{
+	
+}
+
+void AMyPlayerState::OnRep_MyScore()
+{
+	APlayerController* PC = GetPlayerController();
+	if (PC && PC->IsLocalController())
+	{
+		AHUD* HUD = GetWorld()->GetFirstPlayerController()->GetHUD();
+		AMyHUD* MyHUD = Cast<AMyHUD>(HUD);
+		if(MyHUD)
+		{
+			UMyMainWidget* MainWidget = Cast<UMyMainWidget>(MyHUD->GetMainWidget());
+			if(MainWidget)
+			{
+				//MainWidget->UpdateScoreText(MyScore);
+			}
+		}
+	}
+}
+
+void AMyPlayerState::OnRep_MyName()
+{
+	
+}
--- Source/Net_PJB/Public/Core/MyPlayerState.h
+++ Source/Net_PJB/Public/Core/MyPlayerState.h
@@ -5,9 +5,75 @@
 
 #include "MyPlayerState.generated.h"
 
+UENUM(BlueprintType)
+enum class EReadyType : uint8
+{
+	READY			UMETA(DisplayName = "Ready"),
+	NOT_READY		UMETA(DisplayName = "Not Ready")
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnScoreChangedDelegate, int32, NewScore);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnNameChangedDelegate, const FString&, NewName);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnStateChangedDelegate, EReadyType, NewState);
+
+
 UCLASS()
 class NET_PJB_API AMyPlayerState : public APlayerState
 {
 	GENERATED_BODY()
 	
+public:
+	AMyPlayerState();
+
+	FOnScoreChangedDelegate OnScoreChangedDel;
+	FOnNameChangedDelegate OnNameChangedDel;
+	FOnStateChangedDelegate OnStateChangedDel;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "My")
+	void SetReadyType(EReadyType NewReadyType);
+
+	UFUNCTION(BlueprintCallable, Category = "My")
+	void AddMyScore(int32 ScoreToAdd);
+
+	UFUNCTION(BlueprintCallable, Category = "My")
+	void ResetMyScore();
+	
+	UFUNCTION(BlueprintCallable, Category = "My")
+	void SetMyName(const FString& NewName);
+
+
+	UFUNCTION(BlueprintCallable, Category = "My")
+	EReadyType GetReadyType() const { return ReadyType; }
+
+	UFUNCTION(BlueprintCallable, Category = "My")
+	int32 GetMyScore() const { return MyScore; }
+
+	UFUNCTION(BlueprintCallable, Category = "My")
+	FString GetMyName() const { return MyName; }
+
+protected:
+	UFUNCTION()
+	void OnRep_ReadyType();
+	
+	UFUNCTION()
+	void OnRep_MyScore();
+	
+	UFUNCTION()
+	void OnRep_MyName();
+
+protected:
+	UPROPERTY(ReplicatedUsing = OnRep_ReadyType, BlueprintReadWrite, Category = "My")
+	EReadyType ReadyType = EReadyType::NOT_READY;
+
+	UPROPERTY(ReplicatedUsing = OnRep_MyScore, BlueprintReadOnly, Category = "My")
+	int32 MyScore = 0;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_MyName, BlueprintReadOnly, Category = "My")
+	FString MyName;
 };
